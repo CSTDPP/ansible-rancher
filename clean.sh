a1904c36a4292c5926ab1798a985075cafa8b5af
Boris Bathow <boris.bathow@senacor.com>
2020-12-31 07:11:48 +0100

    first working

---
+++ clean.sh
@@ -0,0 +1,7 @@
+rm -rf .cache/
+rm -rf ./plays/darwin-amd64/
+rm -rf ./plays/config-cluster_rancher.*
+rm -rf ./plays/kube_config_config-cluster_rancher.yml
+rm -rf ./plays/helm
+rm -rf ./plays/kubectl-v1.18.0
+rm -rf ./plays/rke-v1.1.4
